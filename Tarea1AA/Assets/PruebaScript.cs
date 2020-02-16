f22878d08e14c71fe294c63150caed6038646f4d
Jose Gamboa <joalgamro2010@gmail.com>
2020-02-16 12:27:19 -0600

    i dont know

--- Tarea1AA/Assets/PruebaScript.cs
+++ Tarea1AA/Assets/PruebaScript.cs
@@ -26,19 +26,16 @@ public class PruebaScript : MonoBehaviour
 
         int temp = 0;
 
-        for (int write = 0; write < arr.Length; write++)
-        {
-            for (int sort = 0; sort < arr.Length - 1; sort++)
-            {
-                if (arr[sort] > arr[sort + 1])
-                {
+        for (int write = 0; write < arr.Length; write++) {
+            for (int sort = 0; sort < arr.Length - 1; sort++) {
+                if (arr[sort] > arr[sort + 1]) {
                     temp = arr[sort + 1];
                     arr[sort + 1] = arr[sort];
                     arr[sort] = temp;
                 }
             }
-
         }
+
 
         return arr;
     }
